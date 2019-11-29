ab372856739195afba584cb17b224a5864525a5c
V1nChy <593386778@qq.com>
2019-11-29 16:29:40 +0800

    commit test project

--- README.md
+++ README.md
@@ -3,24 +3,3 @@
 * version: 2017.4.16f1
 
 * from: https://bitbucket.org/Unity-Technologies/ui
-
-### started ###
-* Clone this repository onto a location on your computer.
-* Configure your IDE for the Unity coding standard, look in the .editorconfig file for more information
-* Open the project in Visual Studio or MonoDevelop
-    * If you are using MonoDevelop
-        * Ensure you enable XBuild (Preferences -> Projects -> Build ->"Compile projects using MSBuild/XBuild")
-        * You may need to restart MonoDevelop
-    * Build the solution
-
-* A folder will be created in the root directory called "Output", the generated dll's will output here in the correct folder structure
-    * If you wish to use these dll's
-    * Locate your Unity install location
-    * Windows: Copy the contents of Output folder to: `Data\UnityExtensions\Unity\GUISystem\{UNITY_VERSION}`
-    * OSX: Copy the contents of Output folder to: `Unity.app/Contents/UnityExtensions/Unity/GUISystem/{UNITY_VERSION}`
-
-* If you want the dll's to copy automatically on build
-    * For each visual studio project file
-        * Open the file in a text editor
-        * Locate the section: <Target Name="AfterBuild">
-        * Follow the instructions in the comments
---
+++ UISystem/UnityEngine.UI-Editor/UI/Core/ImageExtent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityEngine.UI
+{
+    public class ImageExtent:Image
+    {
+        private float _alpha = 1.0F;
+        private Outline[] _outLine;
+        public float alpha
+        {
+            get { return _alpha; }
+            set { SetAlpha(value); }
+        }
+        private void SetAlpha(float value)
+        {
+            _alpha = value;
+            color = new UnityEngine.Color(color.r, color.g, color.b, _alpha);
+        }
+
+        private bool _gray = false;
+        public bool gray
+        {
+            get { return _gray; }
+            set { SetGray(value); }
+        }
+        private void SetGray(bool value)
+        {
+            _gray = value;
+            if (_gray)
+            {
+                material = Resources.Load("material/Gray") as Material;
+            }
+            else
+            {
+                material = null;
+            }
+            if (_outLine == null)
+            {
+                _outLine = GetComponentsInChildren<Outline>(true);
+            }
+            foreach (var line in _outLine)
+            {
+                line.enabled = !value;
+            }
+        }
+    }
+}
